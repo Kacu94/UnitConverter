997b703cef9e66e261a13e5ea0d61837832dabca
kacu94@gmail.com <nokian95>
2017-11-29 21:02:24 +0100

    Renamed method startMessage() -> showStartMessage() Transferred from Description.java to MainConversion.java Deleted Description.java

--- src/First/MainConversion.java
+++ src/First/MainConversion.java
@@ -4,8 +4,18 @@ public class MainConversion {
 
     public static void main(String[] args) {
 
-        Description.startMessage();
+        showStartMessage();
         System.out.println("\r");
         Conversion.console();
+    }
+
+    private static void showStartMessage(){
+
+        System.out.println("Hello and Welcome!");
+        System.out.println("You can convert units like:");
+        System.out.println("Inches, foots, miles yards, ");
+        System.out.println("milimeters, centimeters, meters and kilometers,");
+        System.out.println("in both directions.");
+        System.out.println("Just type first two letters.");
     }
 }
